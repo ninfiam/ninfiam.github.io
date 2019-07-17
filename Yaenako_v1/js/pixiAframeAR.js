174100ad241675b38e4ed2aea57591723e320b20
ninfiam <50287893+ninfiam@users.noreply.github.com>
2019-07-17 21:30:21 +0700

    Update pixiAframeAR.js

--- Yaenako_v1/js/pixiAframeAR.js
+++ Yaenako_v1/js/pixiAframeAR.js
@@ -140,10 +140,10 @@ window.onload = function () {
 		var plane = document.createElement('a-plane');
 		plane.setAttribute('plane', '');
 		plane.setAttribute('color', '#000');
-		plane.setAttribute('height', '9');
-		plane.setAttribute('width', '9');
+		plane.setAttribute('height', '6');
+		plane.setAttribute('width', '6');
 		//マーカーを基準にしたモデルの相対位置
-		plane.setAttribute('position', '0 0 0');
+		plane.setAttribute('position', '1.5 0 0');
 		var stand = stand_mode ? '0 0 0' : '-90 0 0';
 		plane.setAttribute('rotation', stand);
 		marker.appendChild(plane);

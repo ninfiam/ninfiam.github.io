9ecbcc6d945521d0df4fe119c54ea243ef0b6ba6
ninfiam <50287893+ninfiam@users.noreply.github.com>
2019-09-06 12:35:39 +0700

    Update pixiAframeAR.js

--- Saaya/js/pixiAframeAR.js
+++ Saaya/js/pixiAframeAR.js
@@ -143,7 +143,7 @@ window.onload = function () {
 		plane.setAttribute('height', '7');
 		plane.setAttribute('width', '7');
 		//マーカーを基準にしたモデルの相対位置
-		plane.setAttribute('position', '1.4 -2 1');
+		plane.setAttribute('position', '1.4 -2 -0.5');
 		var stand = stand_mode ? '0 0 0' : '-45 0 0';
 		plane.setAttribute('rotation', stand);
 		marker.appendChild(plane);

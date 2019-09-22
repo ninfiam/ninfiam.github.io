bcbcd5fbbb52333ebd9a1c140e877f1124d280ea
ninfiam <50287893+ninfiam@users.noreply.github.com>
2019-09-22 19:19:49 +0700

    Update pixiAframeAR.js

--- Mnakko/js/pixiAframeAR.js
+++ Mnakko/js/pixiAframeAR.js
@@ -143,7 +143,7 @@ window.onload = function () {
 		plane.setAttribute('height', '5');
 		plane.setAttribute('width', '5');
 		//マーカーを基準にしたモデルの相対位置
-		plane.setAttribute('position', '0 0 0');
+		plane.setAttribute('position', '1 0 0');
 		var stand = stand_mode ? '0 0 0' : '-90 0 0';
 		plane.setAttribute('rotation', stand);
 		marker.appendChild(plane);
